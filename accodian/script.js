ece9381fef957ecf1e399ba827daa478c9a42c79
gridbrown <59910838+gridbrown@users.noreply.github.com>
2020-08-05 14:07:58 +0900

    0805

--- accodian/script.js
+++ accodian/script.js
@@ -3,12 +3,6 @@ const heading = document.querySelectorAll(".panel-heading");
 const question = document.querySelectorAll('.panel-question');
 const answer = document.querySelectorAll('.panel-body');
 
-
-function clickEvent() {
-
-}
-
-
 for (let i = 0; i < heading.length; i++) {
     heading[i].addEventListener("click", function (e) {
         for (let j = 0; j < question.length; j++) {
@@ -17,4 +11,27 @@ for (let i = 0; i < heading.length; i++) {
             activateBody(); //작성해야함
         }
     })
+};
+
+
+function activateBody() {
+    // panel-body 모두 안보이도록    
+    for (let x = 0; x < answer.length; x++) {
+        answer[x].style.display = "none";
+
+    }
+    // active된 애의 자식중 panel-body가 나오도록
+    const activePanel = document.querySelector(".panel-question.active .panel-body");
+    activePanel.style.display = "block";
+
 }
+
+
+function collapse() {
+    for (let i = 0; i < answer.length; i++) {
+        answer[i].style.display = "none";
+    }
+}
+
+btnCollaspse.addEventListener("click", collapse);
+activateBody();
